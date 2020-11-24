18de77d9bb72db3564ea018fcb67d8e6999a04de
cristiansk8 <cdpsk8@gmail.com>
2020-11-24 16:16:41 -0500

    footer foro

--- wp-content/themes/dep_asocajas-new/includes/forum/_footer.php
+++ wp-content/themes/dep_asocajas-new/includes/forum/_footer.php
@@ -1,10 +1,10 @@
-<section>
-  <div class="footer-foro container">
+<section class="footer-foro">
+  <div class="footer-foro__content container">
     <div class="row">
-      <div class="col-md-6">
-        <img src="https://www.asocajas.org.co/wp-content/uploads/2020/11/foro-logo.png" alt="">
+      <div class="col-md-6 footer-foro__content-logo">
+        <img src="https://www.asocajas.org.co/wp-content/uploads/2020/11/logo-foro.png" alt="">
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 footer-foro__content-info">
         <p> <span>Para más información:</span> </p>
         <p>foro@asocajas.org.co comunicaciones@asocajas.org.co</p>
       </div>
