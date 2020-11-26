270af78be01b770c49afad056081af433fd81712
cristiansk8 <cdpsk8@gmail.com>
2020-11-26 13:42:24 -0500

    hr ajustado

--- wp-content/themes/dep_asocajas-new/includes/forum/_agenda.php
+++ wp-content/themes/dep_asocajas-new/includes/forum/_agenda.php
@@ -77,7 +77,7 @@ if ($agenda_items): ?>
                 endwhile;
             endif; ?>
           </div>
-          <?php if($hr) ?>
+          <?php if($hr): ?>
           <hr>
         <?php endif; ?>
         </div>
